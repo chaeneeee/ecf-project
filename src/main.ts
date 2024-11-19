ced1983299fe59c9b9e32f9bfb1e75edfb8a9567
chaeneeee <cheche0804@naver.com>
2024-11-20 02:30:26 +0900

    main  수정

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SearchModule } from './search/search.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()  //swagger 로 문서화하기
-    .setTitle('API 명세서') // 문서 제목
-    .setDescription('API 명세서 설명') // 문서 설명
+    .setTitle('ECF API 명세서') // 문서 제목
+    .setDescription('ECF 과제를 위한 API 문서') // 문서 설명
     .setVersion('1.0') // 버전
     .build();
-    const document = SwaggerModule.createDocument(app, config);
+    const document = SwaggerModule.createDocument(app, config, {
+      include: [SearchModule], // 포함할 모듈만 명시
+    } );
     SwaggerModule.setup('ecf-api-docs', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
